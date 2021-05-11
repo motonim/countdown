064b2853cf4cdeed5684f80bcf51598a3d177096
Jaeri Park <motonimjrp@gmail.com>
2021-05-11 00:03:00 -0400

    finishing js

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const countdown = () => {
   const textHour = Math.floor((gap % day) / hour);
   const textMinute = Math.floor((gap % hour) / minute);
   const textSecond = Math.floor((gap % minute) / second);
-  console.log(textDay, textHour, textMinute, textSecond);
+
+  document.querySelector('.day').innerText = textDay;
+  document.querySelector('.hour').innerText = textHour;
+  document.querySelector('.minute').innerText = textMinute;
+  document.querySelector('.second').innerText = textSecond;
 };
 
-countdown();
+setInterval(countdown, 1000);
