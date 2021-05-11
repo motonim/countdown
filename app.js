f6667d5786efb8d6f21613ae1b3ac51172493c37
Jaeri Park <motonimjrp@gmail.com>
2021-05-11 00:24:39 -0400

    adding text animation

--- app.js
+++ app.js
@@ -21,3 +21,31 @@ const countdown = () => {
 };
 
 setInterval(countdown, 1000);
+
+// text animation
+const text = document.querySelector('h2');
+const strText = text.textContent;
+const splitText = strText.split('');
+text.textContent = '';
+
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += '<span>' + splitText[i] + '</span>';
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
